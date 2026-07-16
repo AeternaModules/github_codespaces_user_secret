6981cde62ada69ba198b02fdb2956f8a0d5007c2
Daniel Allen <dan@aeternagroup.net>
2026-07-16 15:01:56 +0100

    Update to schema version v6.13.0

--- outputs.tf
+++ outputs.tf
@@ -1,31 +1,31 @@
 output "codespaces_user_secrets_id" {
   description = "Map of id values across all codespaces_user_secrets, keyed the same as var.codespaces_user_secrets"
-  value       = { for k, v in github_codespaces_user_secret.codespaces_user_secrets : k => v.id }
+  value       = { for k, v in github_codespaces_user_secret.codespaces_user_secrets : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "codespaces_user_secrets_created_at" {
   description = "Map of created_at values across all codespaces_user_secrets, keyed the same as var.codespaces_user_secrets"
-  value       = { for k, v in github_codespaces_user_secret.codespaces_user_secrets : k => v.created_at }
+  value       = { for k, v in github_codespaces_user_secret.codespaces_user_secrets : k => v.created_at if v.created_at != null && length(v.created_at) > 0 }
 }
 output "codespaces_user_secrets_encrypted_value" {
   description = "Map of encrypted_value values across all codespaces_user_secrets, keyed the same as var.codespaces_user_secrets"
-  value       = { for k, v in github_codespaces_user_secret.codespaces_user_secrets : k => v.encrypted_value }
+  value       = { for k, v in github_codespaces_user_secret.codespaces_user_secrets : k => v.encrypted_value if v.encrypted_value != null && length(v.encrypted_value) > 0 }
   sensitive   = true
 }
 output "codespaces_user_secrets_plaintext_value" {
   description = "Map of plaintext_value values across all codespaces_user_secrets, keyed the same as var.codespaces_user_secrets"
-  value       = { for k, v in github_codespaces_user_secret.codespaces_user_secrets : k => v.plaintext_value }
+  value       = { for k, v in github_codespaces_user_secret.codespaces_user_secrets : k => v.plaintext_value if v.plaintext_value != null && length(v.plaintext_value) > 0 }
   sensitive   = true
 }
 output "codespaces_user_secrets_secret_name" {
   description = "Map of secret_name values across all codespaces_user_secrets, keyed the same as var.codespaces_user_secrets"
-  value       = { for k, v in github_codespaces_user_secret.codespaces_user_secrets : k => v.secret_name }
+  value       = { for k, v in github_codespaces_user_secret.codespaces_user_secrets : k => v.secret_name if v.secret_name != null && length(v.secret_name) > 0 }
 }
 output "codespaces_user_secrets_selected_repository_ids" {
   description = "Map of selected_repository_ids values across all codespaces_user_secrets, keyed the same as var.codespaces_user_secrets"
-  value       = { for k, v in github_codespaces_user_secret.codespaces_user_secrets : k => v.selected_repository_ids }
+  value       = { for k, v in github_codespaces_user_secret.codespaces_user_secrets : k => v.selected_repository_ids if v.selected_repository_ids != null && length(v.selected_repository_ids) > 0 }
 }
 output "codespaces_user_secrets_updated_at" {
   description = "Map of updated_at values across all codespaces_user_secrets, keyed the same as var.codespaces_user_secrets"
-  value       = { for k, v in github_codespaces_user_secret.codespaces_user_secrets : k => v.updated_at }
+  value       = { for k, v in github_codespaces_user_secret.codespaces_user_secrets : k => v.updated_at if v.updated_at != null && length(v.updated_at) > 0 }
 }
 
